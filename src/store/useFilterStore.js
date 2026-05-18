faae944aeb1e236837ee124ad989d630fe396e14
Elzar <elzar.eduard@gmail.com>
2026-05-18 19:53:22 +0600

    четвертое задание

--- src/store/useFilterStore.js
+++ src/store/useFilterStore.js
@@ -1,11 +1,19 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useFilterStore = create((set) => ({
-  title: '',
-  categoryId: '',
-  price: '',
-  
-  setFilter: (key, value) => set((state) => ({ ...state, [key]: value })),
-  
-  resetFilters: () => set({ title: '', categoryId: '', price: '' }),
-}));
+export const useFilterStore = create(
+  persist(
+    (set) => ({
+      title: '',
+      categoryId: '',
+      price: '',
+
+      setFilter: (key, value) => set((state) => ({ ...state, [key]: value })),
+
+      resetFilters: () => set({ title: '', categoryId: '', price: '' }),
+    }),
+    {
+      name: 'catalog-filters',
+    }
+  )
+);
